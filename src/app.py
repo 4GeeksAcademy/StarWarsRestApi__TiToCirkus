be65319e2ba98f6b5bac9b1e9f5171965ba17963
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-10-30 19:57:11 +0000

    finalizado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +36,463 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# USERS
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/users", methods=["GET"])
+def get_users():
+    try:
+        query_results = User.query.all()
+        results = list(map(lambda user: user.serialize(), query_results))
+    
+        response_body ={
+            "msg": "Hello, this is your GET /users response ",
+            "results":results
+        }
 
-    return jsonify(response_body), 200
+        return jsonify(response_body), 200
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+    
 
-# this only runs if `$ python src/app.py` is executed
+@app.route("/users", methods=["POST"])
+def add_users():
+    try:
+        data = request.json
+        print(data)
+        existing_user = User.query.filter((User.email == data.get("email")) | (User.name == data.get("name"))).first()
+    
+        if existing_user:
+            return jsonify({"msg": "user already exists"}), 400
+
+        new_user = User(
+            name=data.get("name"),
+            email=data.get("email")
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"msg": "User added successfully", "user": new_user.serialize()}), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+@app.route("/user/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    try:
+        query_result = User.query.filter_by(id = user_id).first()
+        print(query_result)
+        response_body = {
+            "msg": "Hello, this is your GET /user/user_id response ",
+            "result":query_result.serialize()
+        }
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+
+@app.route("/user/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    try:
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"msg": "User not found"}), 404
+        
+        db.session.delete(user)
+        db.session.commit()
+        
+        return jsonify({"msg": "User deleted successfully"}), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+@app.route("/user/<int:user_id>", methods=["PUT"])
+def edit_user(user_id):
+    try:
+        user = User.query.get(user_id)
+
+        if not user:
+            return jsonify({"msg": "User not found"}), 404
+
+        data = request.json
+
+        existing_user = User.query.filter(User.email == data.get("email"), User.id != user_id).first()
+        if existing_user:
+            return jsonify({"msg":"Another user with that email already exists"}), 400
+        
+        if "name" in data:
+            user.name = data["name"]
+        if "email" in data:
+            user.email = data["email"]
+
+        db.session.commit()
+        
+        return jsonify({"msg": "User updated successfully", "user": user.serialize()}), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+# CHARACTERS
+
+@app.route("/characters", methods=["GET"])
+def get_characters():
+    try:
+        query_results  = Character.query.all()
+        results = list(map(lambda character: character.serialize(), query_results))
+        print(results)
+
+        response_body = {
+            "msg": "Hello, this is your GET /characters response ",
+            "results":results
+        }
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+@app.route("/character/<int:character_id>", methods=["GET"])
+def get_character(character_id):
+    try:
+        query_result = Character.query.filter_by(id = character_id).first()
+        print(query_result)
+        response_body = {
+            "msg": "Hello, this is your GET /character/character_id response ",
+            "result":query_result.serialize()
+        }
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+@app.route("/character", methods=["POST"])
+def add_character():
+    try:
+        data = request.get_json()
+
+        name = data.get("name")
+     
+        planet_id = data.get("planet_id")
+
+      
+
+        new_character = Character(name=name,  planet_id=planet_id)
+
+        db.session.add(new_character)
+        db.session.commit()
+
+        return jsonify({"msg": "Personaje creado", "character": new_character.serialize()}), 201
+    except Exception as e:
+        return jsonify({"error": "Error al crear personaje", "message": str(e)}), 500
+
+
+@app.route("/character/<int:character_id>", methods=["PUT"])
+def edit_character(character_id):
+    try:
+        character = Character.query.filter_by(id = character_id).first()
+
+        if not character:
+            return jsonify({"error":"Personaje no encontrado"}), 404
+        
+        data = request.get_json()
+
+        name = data.get("name")
+       
+        planet_id = data.get("planet_id")
+
+        if name:
+            character.name = name
+        
+        if planet_id:
+            character.planet_id = planet_id
+
+        db.session.commit()
+
+        return jsonify({"msg":"Personaje actualizado", "personaje": character.serialize()}), 200
+    except Exception as e:
+        return jsonify({"error":"Error al actualizar personaje", "message": str(e)}), 500
+    
+@app.route("/character/<int:character_id>", methods=["DELETE"])
+def delete_character(character_id):
+    try:
+        character = Character.query.filter_by(id=character_id).first()
+
+        if not character:
+            return jsonify({"error":"Personaje no encontrado"}), 404
+        
+        db.session.delete(character)
+        db.session.commit()
+
+        return jsonify({"msg":f"Personaje {character.name}, eliminado"}), 200
+    except Exception as e:
+        return jsonify({"error": "Error al eliminar personaje", "message": str(e)}), 500
+
+# PLANETS
+
+@app.route("/planets", methods=["GET"])
+def get_planets():
+    try:
+        query_results = Planet.query.all()
+        results = list(map(lambda planet: planet.serialize(), query_results))
+        print(results, "Soy el print planet")
+        
+        response_body = {
+            "msg": "Hello, this is your GET /planets response ",
+            "results":results
+        }
+        return jsonify(response_body), 200
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+    
+@app.route("/planet/<int:planet_id>", methods=["GET"] )
+def get_planet(planet_id):
+    try:
+        query_result = Planet.query.filter_by(id = planet_id).first()
+        print(query_result, "Soy el print de PLanet ID")
+        response_body = {
+            "msg": "hello, this is your GET /planet/planet_id response",
+            "result": query_result.serialize()
+        }
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        return jsonify({"erroe":"Internal error", "message": str(e)}), 500
+
+@app.route("/planet", methods=["POST"])
+def add_planet():
+    try:
+        data = request.get_json()
+
+        name = data.get("name")
+        clima = data.get("clima")
+
+        if  not name or not clima:
+            return jsonify({"error":"Faltan datos"}), 400
+        
+        new_planet = Planet(name =name, clima = clima)
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return jsonify({"msg": "Planeta creado", "planet": new_planet.serialize()}), 201
+
+    except Exception as e:
+        return jsonify({"error": "Error al crear planeta", "message": str(e)}), 500
+
+@app.route("/planet/<int:planet_id>", methods=["PUT"])
+def edit_planet(planet_id):
+    try:
+        planet = planet.query.filter_by(id = planet_id).first()
+
+        if not planet:
+            return jsonify({"error":"Planeta no encontrado"}), 404
+        
+        data = request.get_json()
+
+        name = data.get("name")
+        clima = data.get("clima")
+
+        if name:
+            planet.name = name
+        if clima:
+            planet.clima = clima
+
+        db.session.commit()
+
+        return jsonify({"msg":"Planeta actualizado", "planeta": planet.serialize()}), 200
+    except Exception as e:
+        return jsonify({"error":"Error al actualizar Planeta", "message": str(e)}), 500
+    
+@app.route("/planet/<int:planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    try:
+        planet = Planet.query.filter_by(id=planet_id).first()
+
+        if not planet:
+            return jsonify({"error":"Planeta no encontrado"}), 404
+        
+        db.session.delete(planet)
+        db.session.commit()
+
+        return jsonify({"msg":f"Planeta {planet.name}, eliminado"}), 200
+    except Exception as e:
+        return jsonify({"error": "Error al eliminar Planeta", "message": str(e)}), 500
+
+# FAVORITES
+
+@app.route("/favorites", methods=["GET"])
+def get_favorites():
+    try:
+        query_results = Favorite.query.all()
+        results = list(map(lambda favorite: favorite.serialize(), query_results))
+        response_body = {
+            "msg": "Hello, this is your GET /favorites response",
+            "result": results
+        }
+
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+@app.route("/user/<int:user_id>/favorites", methods=["GET"])
+def get_user_favorites(user_id):
+    try:
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+        favorites = Favorite.query.filter_by(user_id=user_id).all()
+        results = list(map(lambda favorite: favorite.serialize(), favorites))
+        response_body = {
+            "msg": "Hello, this is your GET /user/<user_id>/favorites response",
+            "result": results
+        }
+
+        return jsonify(response_body), 200
+    
+    except Exception as e:
+        return jsonify({"error":"Internal error", "message": str(e)})
+
+# POST
+
+@app.route("/favorite/character/<int:character_id>", methods=["POST"])
+def add_favorite_character(character_id):
+    try:
+        user_id = request.json.get("user_id")
+        if not user_id:
+            return jsonify({"error": "User ID is required"}), 400
+        
+        user = User.query.get("user_id")
+        if user is None:
+            return jsonify({"erroe": "User not found"}), 404
+
+        character = Character.query.get("character_id")
+        if character is None:
+            return jsonify({"error":"Character not found"}), 404
+        
+        existing_favorite = Favorite.query.filter_by(user_id = user_id, character_id= character_id).first()
+        if existing_favorite:
+            return jsonify({"msg":"Character already exists in user favorites"}), 200
+
+        new_favorite = Favorite(user_id=user_id, character_id=character_id)
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Character added to favorites",
+            "favorite": new_favorite.serialize()
+        }
+        return jsonify(response_body), 201
+
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+    
+
+@app.route("/favorite/planet/<int:planet_id>", methods=["POST"])
+def add_favorite_planet(planet_id):
+    try:
+        user_id = request.json.get("user_id")
+        if not user_id:
+            return jsonify({"error":"User ID is required"}), 400
+        
+        # Verifico que exista usuario y planeta
+        user = User.query.get("user_id")
+        if user is None:
+            return jsonify({"error": "User not found"}), 404
+        
+        planet = Planet.query.get("planet_id")
+        if planet is None:
+            return jsonify({"error":"Planet not found"}), 404
+        
+        # Compruebo si planeta ya se encuentra como favorito
+        existing_favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+        if existing_favorite:
+            return jsonify({"msg": "planet already exists in user favorites"}), 200
+        
+        # Creamos un nuevo favorito
+        new_favorite = Favorite(user_id=user_id, planet_id=planet_id)
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Planet added to favorites",
+            "favorite": new_favorite.serialize()
+        }
+        return jsonify(response_body), 201
+
+    except Exception as e:
+        return jsonify({"error": "Internal error", "message": str(e)}), 500
+
+
+
+
+# DELETE
+
+@app.route("/favorite/planet/<int:planet_id>", methods=["DELETE"])
+def delete_favorite_planet(planet_id):
+    try:
+        user_id = request.json.get("user_id")
+        if not user_id:
+            return jsonify({"error": "Se requiere el ID del usuario"}), 400
+   
+        user = User.query.get(user_id)
+        if user is None:
+            return jsonify({"error": "Usuario no encontrado"}), 404
+        
+        planet = Planet.query.get(planet_id)
+        if planet is None:
+            return jsonify({"error": "Planeta no encontrado"}), 404
+  
+        favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+        if not favorite:
+            return jsonify({"error": "Favorito no encontrado"}), 404
+        
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({"msg": "Planeta eliminado de favoritos"}), 200
+
+    except Exception as e:
+        return jsonify({"error": "Error interno", "message": str(e)}), 500
+
+
+@app.route("/favorite/character/<int:character_id>", methods=["DELETE"])
+def delete_favorite_character(character_id):
+    try:
+        user_id = request.json.get("user_id")
+        if not user_id:
+            return jsonify({"error": "Se requiere el ID del usuario"}), 400
+        
+        user = User.query.get(user_id)
+        if user is None:
+            return jsonify({"error": "Usuario no encontrado"}), 404
+        
+        character = Character.query.get(character_id)
+        if character is None:
+            return jsonify({"error": "Personaje no encontrado"}), 404
+        
+        favorite = Favorite.query.filter_by(user_id=user_id, character_id=character_id).first()
+        if not favorite:
+            return jsonify({"error": "Favorito no encontrado"}), 404
+        
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({"msg": "Personaje eliminado de favoritos"}), 200
+
+    except Exception as e:
+        return jsonify({"error": "Error interno", "message": str(e)}), 500
+
+
+
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -4,16 +4,67 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    favorites = db.relationship("Favorite", backref="user", lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=False)
+    favorites = db.relationship("Favorite", backref="character", lazy=True)
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "planet_id": self.planet_id
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    clima = db.Column(db.String(100), nullable=False)
+    characters = db.relationship("Character", backref="planet", lazy=True)
+    favorites = db.relationship("Favorite", backref="planet", lazy=True)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "clima": self.clima
+        }
+    
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable = True)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"), nullable = True)
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id
         }
